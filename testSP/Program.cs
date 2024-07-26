a9d9ad98e0b4c4bdee4a5d2224ae96e6e3afabf9
frei480 <frei741@gmail.com>
2024-07-26 23:55:53 +0500

    headers for kits section

--- testSP/Program.cs
+++ testSP/Program.cs
@@ -1,8 +1,7 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using TFlex.DOCs.Model.Macros;
 using TFlex.DOCs.Model.Macros.ObjectModel;
 using TFlex.Reporting.CAD.MacroGenerator.Macros;
@@ -12,6 +11,7 @@ using TFlex.DOCs.Model.References.Nomenclature;
 using System.Linq.Expressions;
 
 using TFlex.Reporting.CAD.MacroGenerator.ObjectModel;
+using System.Text;
 
 public class Macro : ReportBaseMacroProvider
 {
@@ -28,12 +28,12 @@ public class Macro : ReportBaseMacroProvider
         }
     }
     public class Record
-    { 
-        private Dictionary<string, Cell> cells; 
+    {
+        private Dictionary<string, Cell> cells;
         public bool isHeader = false;
         public Record(bool _isHeader = false)
         {
-                cells = new Dictionary<string, Cell>()
+            cells = new Dictionary<string, Cell>()
                 {
                     { "Формат", new Cell("Ф", new Guid("42a5dd37-e537-46ab-88d2-97060ca46c1c")) },
                     { "Зона", new Cell("З", new Guid("1367dda9-7850-4c15-b123-636ab692034c")) },
@@ -53,9 +53,7 @@ public class Macro : ReportBaseMacroProvider
     }
     public class SectionList
     {
-
         private Dictionary<string, List<ReferenceObject>> sections;
-            
         public SectionList()
         {
             sections = new Dictionary<string, List<ReferenceObject>>()
@@ -69,10 +67,9 @@ public class Macro : ReportBaseMacroProvider
                 { "Materials",  new List<ReferenceObject>() },
                 { "Kits",  new List<ReferenceObject>() },
             };
-
         }
         public List<ReferenceObject> this[string sectionName]
-        { 
+        {
             get { return sections[sectionName]; }
         }
     }
@@ -105,7 +102,7 @@ public class Macro : ReportBaseMacroProvider
             {"Толщина покрытия", new Guid("4d5f4eed-cc83-41b2-8883-e9a971ef6a44")},
             {"Стандарт", new Guid("ed49d263-9df9-4d6a-8af7-2fa69f448d47")},
         };
-    private static Dictionary<String, String> SingularPluralMap = new Dictionary<string, string>()
+    private static Dictionary<string, string> SingularPluralMap = new Dictionary<string, string>()
         {
             {"Болт", "Болты"},
             {"Винт", "Винты"},
@@ -131,7 +128,7 @@ public class Macro : ReportBaseMacroProvider
             {"Шуруп", "Шурупы"},
         };
 
-    public Macro(ReportGenerationBaseMacroContext context)        : base(context)    {    }
+    public Macro(ReportGenerationBaseMacroContext context) : base(context) { }
     public override void Run()
     {
         System.Diagnostics.Debugger.Launch();
@@ -139,58 +136,58 @@ public class Macro : ReportBaseMacroProvider
         List<Record> records = new List<Record>();
         NomenclatureObject current_obj = (ReferenceObject)CurrentObject as NomenclatureObject;
         List<NomenclatureObject> versions = current_obj.GetVersions();
-        NomenclatureObject base_version = versions[0]; 
+        NomenclatureObject base_version = versions[0];
         var allPart = versions.SelectMany(v => v.Children.OfType<NomenclatureObject>()).ToList();
         var variable_part = new List<NomenclatureObject>();
-        
+
         /*--------------------------------TODO-------------------------------------  
         Не забыть переписать эту хрень на лямбду !  =°,,°=
          -------------------------------------------------------------------------*/
-        foreach (NomenclatureObject obj in allPart) 
+        foreach (NomenclatureObject obj in allPart)
         {
             List<double> quantity = new List<double>();
             foreach (var v in versions)
             {
-                try {
+                try
+                {
                     var i = obj.GetParentLink(v).Id;
-                    quantity.Add( (double)obj.GetParentLink(v)[Guids["Количество"]].Value);
+                    quantity.Add((double)obj.GetParentLink(v)[Guids["Количество"]].Value);
                 }
-                catch (NullReferenceException) 
+                catch (NullReferenceException)
                 {
                     variable_part.Add(obj);
                     quantity.Clear();
                 }
             }
-            if (quantity.Distinct().Count() >1)  { variable_part.Add(obj); }
+            if (quantity.Distinct().Count() > 1) { variable_part.Add(obj); }
         }
 
         var constantPart = allPart.Except(variable_part).ToList();
-        Message("123", $"constant part count {constantPart.Count.ToString()} ");
+
         filling_by_list(constantPart, base_version, records);
         //Переменные данные для исполнений
         records.Add(new Record());
         Record dummy = new Record();
         dummy["Обозначение"].value = "Переменные данные";
         dummy["Наименование"].value = "для исполнений";
-        records.Add(dummy); 
+        records.Add(dummy);
         foreach (var v in versions)
         {
             Record header = new Record(true);
             header["Наименование"].value = v.Denotation;
             records.Add(header);
             var childs = v.Children.OfType<NomenclatureObject>().Except(constantPart).ToList();
-            filling_by_list(childs, v, records);
+            filling_by_list(variable_part, v, records);
         }
-        print_test(records);
     }
     public void filling_by_list(List<NomenclatureObject> section, NomenclatureObject parent, List<Record> rl)
     {
-        SectionList sl = new SectionList();
         parent.Children.Reload();
+        SectionList sl = new SectionList();
+
         foreach (NomenclatureObject obj in section.Where(o => o.GetParentLink(parent) != null))
         {
             var child = obj.GetParentLink(parent).ChildObject;
-            //switch (obj.GetParentLink(parent)[Guids["Раздел спецификации"]].Value.ToString())
             string s = obj.GetParentLink(parent)[Guids["Раздел спецификации"]].Value.ToString();
             switch (s)
             {
@@ -200,21 +197,20 @@ public class Macro : ReportBaseMacroProvider
                 case "Детали": sl["Parts"].Add(child); break;
                 case "Стандартные изделия": sl["StandartParts"].Add(child); break;
                 case "Прочие изделия": sl["MiscParts"].Add(child); break;
-                case "Материалы": sl["Materials"].Add(child); break;                
-                //case "Комплекты": sl["Kits"].Add(child); break;
+                case "Материалы": sl["Materials"].Add(child); break;
                 case { } when s.StartsWith("Комплекты"): sl["Kits"].Add(child); break;
             }
         }
-            
-        rl.AddRange( MakeList( sl["Documentation"].OrderBy(obj => obj[Guids["Обозначение"]].Value).ToList(), parent, "Документация"));
-        rl.AddRange(MakeList( sl["Complexes"].OrderBy(obj => obj[Guids["Обозначение"]].Value).ToList(), parent, "Комплексы"));
-        rl.AddRange(MakeList( sl["Assemblies"].OrderBy(obj => obj[Guids["Обозначение"]].Value).ToList(), parent, "Сборочные единицы"));
-        rl.AddRange(MakeList( sl["Parts"].OrderBy(obj => obj[Guids["Обозначение"]].Value).ToList(), parent, "Детали"));
+
+        rl.AddRange(MakeList(sl["Documentation"].OrderBy(obj => obj[Guids["Обозначение"]].Value).ToList(), parent, "Документация"));
+        rl.AddRange(MakeList(sl["Complexes"].OrderBy(obj => obj[Guids["Обозначение"]].Value).ToList(), parent, "Комплексы"));
+        rl.AddRange(MakeList(sl["Assemblies"].OrderBy(obj => obj[Guids["Обозначение"]].Value).ToList(), parent, "Сборочные единицы"));
+        rl.AddRange(MakeList(sl["Parts"].OrderBy(obj => obj[Guids["Обозначение"]].Value).ToList(), parent, "Детали"));
 
         rl.AddRange(MakeListSti(sl["StandartParts"], parent, "Стандартные изделия"));
-        rl.AddRange(MakeList( sl["MiscParts"], parent, "Прочие изделия"));
-        rl.AddRange(MakeList( sl["Materials"], parent, "Материалы"));
-        rl.AddRange(MakeListKit( sl["Kits"], parent, "Комплекты")); 
+        rl.AddRange(MakeList(sl["MiscParts"], parent, "Прочие изделия"));
+        rl.AddRange(MakeList(sl["Materials"], parent, "Материалы"));
+        rl.AddRange(MakeListKit(sl["Kits"], parent, "Комплекты"));
     }
     public void print_test(List<Record> list)
     {
@@ -225,7 +221,7 @@ public class Macro : ReportBaseMacroProvider
             var текст = Текст["Текст1"];
             var шаблон = текст["Наименование"];
             var строка = текст.Таблица.ДобавитьСтроку(шаблон);
-            if(cl.isHeader)
+            if (cl.isHeader)
             {
                 шаблон = текст["Раздел"];
                 строка = текст.Таблица.ДобавитьСтроку(шаблон);
@@ -234,19 +230,18 @@ public class Macro : ReportBaseMacroProvider
             }
             var cellNames = new List<string> { "Формат", "Зона", "Позиция", "Обозначение", "Наименование", "Кол", "Примечание" };
             foreach (var cellName in cellNames)
-            {                           
-                строка[cl[cellName].tag].Текст = cl[cellName].value; 
+            {
+                строка[cl[cellName].tag].Текст = cl[cellName].value;
             }
         }
     }
-    
     public List<Record> MakeListSti(List<ReferenceObject> list, NomenclatureObject parent, string name_sec)
     {
         if (list == null || list.Count == 0)
         {
             return new List<Record>();
         }
-        //list = list.Distinct().ToList();
+
         var records = new List<Record>();
         Record header = new Record(true);
         header["Наименование"].value = name_sec;
@@ -262,7 +257,7 @@ public class Macro : ReportBaseMacroProvider
         {
             if (group.Count > 1)
             {
-                string NamePart = group.PartList.FirstOrDefault()[Guids["Наименование изделия"]].GetString();
+                string NamePart = group.PartList.FirstOrDefault()[Guids["Наименвоание изделия"]].GetString();
                 string PluralName = SingularPluralMap.ContainsKey(NamePart) ? SingularPluralMap[NamePart] : NamePart;
                 Record groupHeader = new Record();
                 groupHeader["Наименование"].value = PluralName + " " + group.GroupName; ;
@@ -291,75 +286,19 @@ public class Macro : ReportBaseMacroProvider
                 try { item["Примечание"].value = parent.GetChildLink(child)[item["Примечание"].guid].Value.ToString(); }
                 catch (NullReferenceException) { }
                 records.Add(item);
-                }
-           }
-           return records;
-    }
-        
-    public List<Record> MakeListKit(List<ReferenceObject> list, NomenclatureObject v, string name_sec)
-{
-    if (list == null || list.Count == 0)
-    {
-        return new List<Record>();
-    }
-    var records = new List<Record>();
-    Record header = new Record(true);
-    header["Наименование"].value = name_sec;
-    records.Add(header);
-    var kit = list.GroupBy(obj => obj.GetParentLink(v)[Guids["Раздел спецификации"]].Value.ToString())
-                            .Select(g => new
-                            {
-                                GroupName = g.Key,
-                                Count = g.Count(),
-                                PartList = g.Select(p => p)
-                            });
-    foreach (var group in kit)
-    {
-        if (group.Count > 0)
-        {
-            string[] names = group.GroupName.Split('/');
-            if (names.Length > 1)
-            {
-                string headerGroup = names[1];
-                var sb = new StringBuilder();
-                sb.Append(headerGroup[0].ToString().ToUpper());
-                sb.Append(headerGroup.Substring(1));
-                Record groupHeader = new Record();
-                groupHeader["Наименование"].value = sb.ToString(); ;
-                records.Add(groupHeader);
-                records.Add(new Record());
             }
         }
-        foreach (var child in group.PartList.Distinct())
-        {
-            Record item = new Record();
-            try { item["Формат"].value = child[item["Формат"].guid].Value.ToString() ?? ""; }
-            catch (NullReferenceException) { }
-            try { item["Зона"].value = child.GetParentLink(v)[item["Зона"].guid].Value.ToString(); }
-            catch (NullReferenceException) { }
-            try { item["Позиция"].value = child.GetParentLink(v)[item["Позиция"].guid].Value.ToString(); }
-            catch (NullReferenceException) { }
-            try { item["Обозначение"].value = child[item["Обозначение"].guid].Value.ToString(); }
-            catch (NullReferenceException) { }
-            try { item["Наименование"].value = child[item["Наименование"].guid].Value.ToString(); }
-            catch (NullReferenceException) { }
-            try { item["Кол"].value = v.GetChildLinks(child).Sum(link => (double)link[item["Кол"].guid].Value).ToString(); }
-            catch (NullReferenceException) { }
-            try { item["Примечание"].value = v.GetChildLink(child)[item["Примечание"].guid].Value.ToString(); }
-            catch (NullReferenceException) { }
-            records.Add(item);
-        }
+        return records;
     }
-    return records;
-}
+
     private string MakeNameSti(ReferenceObject child)
     {
         string str = string.Empty;
-        string precisionClass = string.IsNullOrEmpty(child[Guids["Класс точности"]].ToString()) ? "": child[Guids["Класс точности"]].ToString();
+        string precisionClass = string.IsNullOrEmpty(child[Guids["Класс точности"]].ToString()) ? "" : child[Guids["Класс точности"]].ToString();
         string variant = string.IsNullOrEmpty(child[Guids["Исполнение"]].ToString()) ? "" : child[Guids["Исполнение"]].ToString();
         string diameter = string.IsNullOrEmpty(child[Guids["D резьбы"]].ToString()) ? "" : "М" + child[Guids["D резьбы"]].ToString();
         string pitch = child[Guids["Шаг резьбы"]].GetDouble() == 0 ? "" : "х" + child[Guids["D резьбы"]].ToString();
-        string leftHanded = string.IsNullOrEmpty(child[Guids["Напр. резьбы"]].ToString()) ? "" : "-"+child[Guids["Напр. резьбы"]].ToString();
+        string leftHanded = string.IsNullOrEmpty(child[Guids["Напр. резьбы"]].ToString()) ? "" : "-" + child[Guids["Напр. резьбы"]].ToString();
         string threadTol = string.IsNullOrEmpty(child[Guids["Поле допуска резьбы"]].ToString()) ? "" : "-" + child[Guids["Поле допуска резьбы"]].ToString();
         string length = string.IsNullOrEmpty(child[Guids["Длина изделия"]].ToString()) ? "" : "-" + child[Guids["Длина изделия"]].ToString();
         string strength = string.IsNullOrEmpty(child[Guids["Класс прочности"]].ToString()) ? "" : "." + child[Guids["Класс прочности"]].ToString();
@@ -368,10 +307,66 @@ public class Macro : ReportBaseMacroProvider
         string coatType = string.IsNullOrEmpty(child[Guids["Вид покрытия"]].ToString()) ? "" : "." + child[Guids["Вид покрытия"]].ToString();
         string coatTickness = child[Guids["Толщина покрытия"]].GetInt32() == 0 ? "" : child[Guids["Толщина покрытия"]].ToString();
 
-        str = precisionClass + variant + diameter +pitch + leftHanded + threadTol + steelType + material + coatType + coatTickness;
+        str = precisionClass + variant + diameter + pitch + leftHanded + threadTol + steelType + material + coatType + coatTickness;
         return str;
     }
 
+    public List<Record> MakeListKit(List<ReferenceObject> list, NomenclatureObject v, string name_sec)
+    {
+        if (list == null || list.Count == 0)
+        {
+            return new List<Record>();
+        }
+        var records = new List<Record>();
+        Record header = new Record(true);
+        header["Наименование"].value = name_sec;
+        records.Add(header);
+        var kit = list.GroupBy(obj => obj.GetParentLink(v)[Guids["Раздел спецификации"]].Value.ToString())
+                                .Select(g => new
+                                {
+                                    GroupName = g.Key,
+                                    Count = g.Count(),
+                                    PartList = g.Select(p => p)
+                                });
+        foreach (var group in kit)
+        {
+            if (group.Count > 0)
+            {
+                string[] names = group.GroupName.Split('/');
+                if (names.Length > 0)
+                {
+                    string headerGroup = names[1];
+                    var sb = new StringBuilder();
+                    sb.Append(headerGroup[0].ToString().ToUpper());
+                    sb.Append(headerGroup.Substring(1));
+                    Record groupHeader = new Record();
+                    groupHeader["Наименование"].value = sb.ToString(); ;
+                    records.Add(groupHeader);
+                    records.Add(new Record());
+                }
+            }
+            foreach (var child in group.PartList.Distinct())
+            {
+                Record item = new Record();
+                try { item["Формат"].value = child[item["Формат"].guid].Value.ToString() ?? ""; }
+                catch (NullReferenceException) { }
+                try { item["Зона"].value = child.GetParentLink(v)[item["Зона"].guid].Value.ToString(); }
+                catch (NullReferenceException) { }
+                try { item["Позиция"].value = child.GetParentLink(v)[item["Позиция"].guid].Value.ToString(); }
+                catch (NullReferenceException) { }
+                try { item["Обозначение"].value = child[item["Обозначение"].guid].Value.ToString(); }
+                catch (NullReferenceException) { }
+                try { item["Наименование"].value = child[item["Наименование"].guid].Value.ToString(); }
+                catch (NullReferenceException) { }
+                try { item["Кол"].value = v.GetChildLinks(child).Sum(link => (double)link[item["Кол"].guid].Value).ToString(); }
+                catch (NullReferenceException) { }
+                try { item["Примечание"].value = v.GetChildLink(child)[item["Примечание"].guid].Value.ToString(); }
+                catch (NullReferenceException) { }
+                records.Add(item);
+            }
+        }
+        return records;
+    }
     public List<Record> MakeList(List<ReferenceObject> list, NomenclatureObject v, string name_sec)
     {
         if (list == null || list.Count == 0)
@@ -397,7 +392,7 @@ public class Macro : ReportBaseMacroProvider
             catch (NullReferenceException) { }
             try { item["Кол"].value = v.GetChildLinks(child).Sum(link => (double)link[item["Кол"].guid].Value).ToString(); }
             catch (NullReferenceException) { }
-            try { item["Примечание"].value = v.GetChildLink(child)[item["Примечание"].guid].Value.ToString(); }                             
+            try { item["Примечание"].value = v.GetChildLink(child)[item["Примечание"].guid].Value.ToString(); }
             catch (NullReferenceException) { }
             records.Add(item);
         }
